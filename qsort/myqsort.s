0077d9542b45ee6f1dccb3bb1d07af4f5df172d2
KKiiim <145949907+KKiiim@users.noreply.github.com>
2023-09-25 16:23:14 +0800

    Update myqsort.s

--- qsort/myqsort.s
+++ qsort/myqsort.s
@@ -1,67 +1,75 @@
-// x0 *arr
-// w1 int left
-// w2 int right
-// w3 int tmp as partition
-// x4 left*4
-// w5 i
-// w6 j
-// w7 i*4
-// w8 j*4
-// x9 tmp
+.globl	QkSort
+QkSort:
+	cmp	w1, w2		
+	b.gt	.end	// left > right 直接结束
 
+	stp	x29, x30, [sp, #-48]!           
+	str	x21, [sp, #16]              
+	mov	x29, sp
+	stp	x20, x19, [sp, #32]          
 
-.globl myqsort
-myqsort:
-    cmp x1, x2
-    b.gt end   // left > right ==> over
+	ldr	w8, [x0, w1, sxtw #2]
+	mov	w19, w2
+	mov	x20, x0
+	sxtw	x9, w1
+	mov	w21, w1
+	cmp	w1, w2
+	b.eq	.LBB0_9
 
-    lsl x3, x1, #2
-    ldr x4, [x0, x3]    // get tmp value
+	mov	x10, #4294967296
+	mov	x11, #-4294967296
+	mov	w13, w1
+	mov	w12, w19
+.LBB0_3:
+	sxtw	x18, w12
+	sxtw	x13, w13
+	add	w12, w12, #1
+	add	x17, x10, x18, lsl #32
+.LBB0_4:
+	ldr	w15, [x20, x18, lsl #2]
+	sub	x16, x18, #1
+	mov	w14, w12
+	add	x17, x17, x11
+	sub	w12, w12, #1
+	cmp	w15, w8
+	ccmp	x18, x13, #4, ge
+	mov	x18, x16
+	b.gt	.LBB0_4
 
-    mov x5, x1
-    mov x6, x2  // get i, j
+	asr	x18, x17, #32
+	add	x17, x20, x13, lsl #2
+.LBB0_6:
+	ldr	w0, [x17], #4
+	add	x2, x13, #1
+	cmp	w0, w8
+	ccmp	x18, x13, #4, le
+	mov	x13, x2
+	b.gt	.LBB0_6
 
-    bl left_loop
-    bl right_loop
+	add	x16, x20, x16, lsl #2
+	sub	w13, w2, #1
+	cmp	w14, w2
+	stur	w15, [x17, #-4]
+	str	w0, [x16, #4]
+	b.ne	.LBB0_3
 
-    lsl x7, x5, #2
-    ldr x9, [x0, x7]
-    str x9, [x0, x3]    // arr[left] = arr[i]
+	mov	w21, w13
+.LBB0_9:
+	sbfiz	x10, x21, #2, #32
+	sub	w2, w21, #1
+	mov	x0, x20
+                 
+	ldr	w11, [x20, x10]
+	str	w11, [x20, x9, lsl #2]
+	str	w8, [x20, x10]
+	bl	QkSort
+	add	w1, w21, #1
+	mov	x0, x20
+	mov	w2, w19
+	bl	QkSort
 
-    str x4, [x0, x7]    // arr[i] = tmp
-
-    sub x8, x5, #1
-    mov x2, x8
-    bl myqsort
-
-    add x5, x5, #1
-    mov x1, x5
-    bl myqsort
-
-    b end
-
-left_loop:
-    ldr x7, [x0, x5, lsl #3]
-    cmp x7, x4
-
-    add x5, x5, #1
-    blt left_loop
-
-    ret
-
-right_loop:
-    lsl x7, x6, #2
-    add x7, x7, x0
-    ldr x7, [x7]
-    cmp x7, x4
-
-    sub x6, x6, #1
-    bge right_loop
-
-    ret
-
-end:
-    ret
-
-//    QkSort(arr, left, i - 1);
-//    QkSort(arr, i + 1, right);
+	ldp	x20, x19, [sp, #32]         
+	ldr	x21, [sp, #16]          
+	ldp	x29, x30, [sp], #48    
+.end:
+	ret
