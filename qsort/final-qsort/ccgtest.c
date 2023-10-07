dc937e525bc127ae502a0709f5561e191c58594e
KKiiim <145949907+KKiiim@users.noreply.github.com>
2023-10-07 15:04:19 +0800

    Update ccgtest.c

--- qsort/final-qsort/ccgtest.c
+++ qsort/final-qsort/ccgtest.c
@@ -26,6 +26,8 @@ void myqsort(int *arr, int left, int right)
 int main(){
     int str[11] = {0, 5, 2, 7, 4, 3, 9, 0, 1, 6, 8};
     myqsort(str, 1, 10);
-    int expected_array[11] = {0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    EXPECT_EQ(expected_array, str);
+    int expected_array[11] = {0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};\
+    for(int i = 0; i < 11; ++i){
+      EXPECT_EQ(expected_array[i], str[i]);
+    }
 }
