43d3f47e1184a953d56399c3848b1e5b97c63fc7
KKiiim <145949907+KKiiim@users.noreply.github.com>
2023-10-11 18:21:30 +0800

    Update qsort.c

--- qsort/final-qsort/qsort.c
+++ qsort/final-qsort/qsort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <gtest/gtest.h>
 
 extern void myqsort(int *arr, int left, int right);
-//extern void QkSort(int *arr, int left, int right);
 
 void printStr(int str[], size_t len)
 {
@@ -14,13 +14,9 @@ void printStr(int str[], size_t len)
 
 int main(){
     int str[11] = {0, 5, 2, 7, 4, 3, 9, 0, 1, 6, 8};
-//    int str[11] = {0,1,2,3,4,5,6,7,8,9};
-    //int str[4] = {1,1,3,2};
-    printf("before qsort\n");
-    printStr(str, 11);
-
     myqsort(str, 1, 10);
-//   QkSort(str, 0, 9);
-    printf("after qsort\n");
-    printStr(str, 11);
+    int expected_array[11] = {0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};\
+    for(int i = 0; i < 11; ++i){
+      EXPECT_EQ(expected_array[i], str[i]);
+    }
 }
