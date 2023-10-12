11d600eb5b00f7e3b82dab24d1bc63fdc2389d82
KKiiim <145949907+KKiiim@users.noreply.github.com>
2023-10-12 10:13:48 +0000

    add ctest

--- ctest-cmake-qsort/src/qsort.cpp
+++ ctest-cmake-qsort/src/qsort.cpp
@@ -12,7 +12,7 @@ void printStr(int str[], size_t len)
     printf("\n");
 }
 
-int main(){
+TEST(MockFileStorageTest, testClearJob) {
     int str[11] = {0, 5, 2, 7, 4, 3, 9, 0, 1, 6, 8};
     printStr(str, 11);
     myqsort(str, 1, 10);
@@ -22,3 +22,14 @@ int main(){
       EXPECT_EQ(expected_array[i], str[i]);
     }
 }
+
+//int main(){
+    // int str[11] = {0, 5, 2, 7, 4, 3, 9, 0, 1, 6, 8};
+    // printStr(str, 11);
+    // myqsort(str, 1, 10);
+    // printStr(str, 11);
+    // int expected_array[11] = {0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // for(int i = 0; i < 11; ++i){
+    //   EXPECT_EQ(expected_array[i], str[i]);
+    // }
+//}
