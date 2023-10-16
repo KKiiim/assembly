21cc069e9c10c1d8b459c2bf5a0db277135b3f7c
Rui Jin (ext.) <rui.jin@partner.bmw.com>
2023-10-16 09:57:04 +0800

    new addwasm, update .gitignore

---
+++ wasm-practice/add2num/add.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+// 异步加载 .wasm 文件
+fs.readFile('mysum.wasm', (error, buffer) => {
+  if (error) {
+    console.error('Failed to load WebAssembly module:', error);
+    return;
+  }
+
+  // 将二进制数据转换为 Uint8Array
+  const wasmCode = new Uint8Array(buffer);
+
+  // 加载并实例化 WebAssembly 模块
+  WebAssembly.instantiate(wasmCode)
+    .then(wasmModule => {
+      // 获取导出的函数
+      const sumFunc = wasmModule.instance.exports.add;
+
+      // 调用函数并获取结果
+      const result = sumFunc(5, 3);
+      console.log(result); // 输出：8
+    })
+    .catch(error => {
+      // 处理实例化错误
+      console.error('Failed to instantiate WebAssembly module:', error);
+    });
+});
