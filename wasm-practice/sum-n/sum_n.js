57a21b0dc061b66b5259f0909b67ab0023c5dab5
Rui Jin (ext.) <rui.jin@partner.bmw.com>
2023-10-16 18:05:06 +0800

    update test-sumn

--- wasm-practice/sum-n/sum_n.js
+++ wasm-practice/sum-n/sum_n.js
@@ -1,27 +1,29 @@
-const fs = require('fs');
+const fs = require("fs");
 
-// 异步加载 .wasm 文件
-fs.readFile('mysum_n.wasm', (error, buffer) => {
-  if (error) {
-    console.error('Failed to load WebAssembly module:', error);
-    return;
-  }
+function getResult() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("mysum_n.wasm", (error, buffer) => {
+      if (error) {
+        console.error("Failed to load WebAssembly module:", error);
+        reject(error);
+        return;
+      }
 
-  // 将二进制数据转换为 Uint8Array
-  const wasmCode = new Uint8Array(buffer);
+      const wasmCode = new Uint8Array(buffer);
 
-  // 加载并实例化 WebAssembly 模块
-  WebAssembly.instantiate(wasmCode)
-    .then(wasmModule => {
-      // 获取导出的函数
-      const nsumFunc = wasmModule.instance.exports.n_sum;
-
-      // 调用函数并获取结果
-      const result = nsumFunc(5);
-      console.log(result); // 输出：15
-    })
-    .catch(error => {
-      // 处理实例化错误
-      console.error('Failed to instantiate WebAssembly module:', error);
+      WebAssembly.instantiate(wasmCode)
+        .then((wasmModule) => {
+          const nsumFunc = wasmModule.instance.exports.n_sum;
+          const result = nsumFunc(5);
+          console.log(result); // 输出：15
+          resolve(result);
+        })
+        .catch((error) => {
+          console.error("Failed to instantiate WebAssembly module:", error);
+          reject(error);
+        });
     });
-});
+  });
+}
+
+module.exports = { getResult };
