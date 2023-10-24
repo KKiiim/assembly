33a838e6564f7e950397c99ff07936d80b8fa0f7
Rui Jin (ext.) <rui.jin@partner.bmw.com>
2023-10-24 09:46:44 +0800

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+  globals: {
+    exports: "readonly",
+    require: "readonly",
+    __dirname: "readonly",
+  },
+};
